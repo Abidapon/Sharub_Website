365b07430b08d672542f1745458fbaca95c9ab7b
rcss <aponabid154@gmail.com>
2023-11-03 03:12:31 +0600

    Added new feature

--- js/main.js
+++ js/main.js
@@ -10,3 +10,16 @@ window.addEventListener('scroll', () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('nav-toggle');
 });
+
+const readMoreButton = document.getElementById("read-more-button");
+const hiddenContent = document.querySelector(".hidden-content");
+
+readMoreButton.addEventListener("click", function () {
+    if (hiddenContent.style.display === "none" || hiddenContent.style.display === "") {
+        hiddenContent.style.display = "block";
+        readMoreButton.textContent = "Read Less";
+    } else {
+        hiddenContent.style.display = "none";
+        readMoreButton.textContent = "Read More";
+    }
+});
